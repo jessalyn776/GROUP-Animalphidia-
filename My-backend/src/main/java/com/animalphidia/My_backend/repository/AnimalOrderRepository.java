a0aeb5768d03f2253e0b224eb11f78f48fe53de2
jessalyn776 <jessalyntrogon@gmail.com>
2025-12-30 01:43:53 +0800

    Create AnimalOrderRepository.java

---
+++ My-backend/src/main/java/com/animalphidia/My_backend/repository/AnimalOrderRepository.java
@@ -0,0 +1,9 @@
+package com.animalphidia.My_backend.repository;
+
+import com.animalphidia.My_backend.model.AnimalOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalOrderRepository extends JpaRepository<AnimalOrder, Integer> {
+}
